0664ab7f0fce55205358a3ed36c4ae32d09b678c
Miguel Esquirol <miguelesquirol@gmail.com>
2019-01-15 18:55:20 +0000

    portfolio pdf

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -2,14 +2,7 @@
  * @title App
  * @description Application entry point
  */
-
-import navbar from '../modules/navbar/navbar';
-import carousel from '../modules/carousel/carousel';
 import theme from './components/theme';
-
-
 document.addEventListener('DOMContentLoaded', function() {
-  navbar();
-  carousel();
   theme();
 })
